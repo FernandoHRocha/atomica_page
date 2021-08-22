1d8da6e0fb9a984ae919867794c49e396e809ebb
Fernando Rocha <fhrlobacz@gmail.com>
2021-08-22 18:42:06 -0300

    criando campos para captação

--- src/components/layout/home.jsx
+++ src/components/layout/home.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { render } from 'react-dom'
 
-function cabecalho() {
-return  <header className="cabecalho_propaganda">
-            <div class='titulo_cabecalho'>
-                <h1>Cafel do <span>ABÉ</span></h1>
-            </div>
-            <nav class='nav_horizontal'>
-                <ul>
-                    <li>
-                        <button onclick="trocarConteudo('conteudo_sobre','conteudo_busca')" class="sobre_tab">
-                            <a href='/'>SOBRE</a>
-                        </button>
-                    </li>
-                    <li>
-                        <button onclick="trocarConteudo('conteudo_busca','conteudo_sobre')" class="busca_tab">
-                            <a href='/'>BUSCAR</a>
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+        };
+        this.onInputchange = this.onInputchange.bind(this);
+        this.onSubmitForm = this.onSubmitForm.bind(this);
+      }
+    
+      onInputchange(event) {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+      }
+    
+      onSubmitForm() {
+        console.log(this.state)
+      }
+
+    render()
+    {   
+    return (
+        <div>
+            <header className="cabecalho">
+                <h1 className="cabecalho__titulo">Cafel do <span>ABÉ</span></h1>
+            </header>
+            <form className="formulario">
+                <label className="formulario__titulo">Conta pra gente</label>
+                <input onChange={this.onInputchange} placeholder='Quem é você' type="text" name="nome" id="formulario_nome"/>
+                <input onChange={this.onInputchange} placeholder='Qual o seu melhor e-mail' type="text" name="email" id="formulario_email"/>
+                <input onClick={this.onSubmitForm} type="button" value="Enviar"/>
+            </form>
+        </div>
+        );
+    }
 }
-
-export default cabecalho
+export default Home;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Home from './components/layout/home'
 
 ReactDOM.render(
   <>
-    <h1>Minha página</h1>
     <Home></Home>
   </>,
   document.getElementById('root')
